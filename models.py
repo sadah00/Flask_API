3a55b11845179fb4af861d0b38075b591240a56c
sadah00 <kimberlysada30@gmail.com>
2025-11-27 17:53:53 +0300

    jwt

--- models.py
+++ models.py
@@ -24,3 +24,10 @@ class Purchases(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)  
     stock_quantity = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
